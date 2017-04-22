b53502dd7dde46368ca24f9a9d69647a71b29064
BartolomeSintes <bartolome.sintes@gmail.com>
2017-04-23 00:25:11 +0200

    Enlaces a EmojiOne son a versión 2.2.7 + incluidos Símbolos misceláneos

--- html/unicode_genera_tabla.php
+++ html/unicode_genera_tabla.php
@@ -78,7 +78,7 @@
               if ($tmp[0] == "0") {
                 $tmp = substr($tmp, 1);
               }
-              print "          <span class=\"eo\"><a href=\"https://github.com/Ranks/emojione/tree/master/assets/svg/$tmp.svg\">&#x" . $c[0][0] . ";</a></span>\n";
+              print "          <span class=\"eo\"><a href=\"https://github.com/Ranks/emojione/tree/2.2.7/assets/svg/$tmp.svg\">&#x" . $c[0][0] . ";</a></span>\n";
             } else {
               print "          <span class=\"eo\">&#x" . $c[0][0] . ";</span>\n";
             }
@@ -105,7 +105,7 @@
               if ($tmp1[0] == "0") {
                 $tmp1 = substr($tmp1, 1);
               }
-              print "          <span class=\"eo\"><a href=\"https://github.com/Ranks/emojione/tree/master/assets/svg/$tmp0-$tmp1.svg\">&#x" . $c[0][0] . ";&#x200D;&#x" . $c[0][1] . ";</a></span>\n";
+              print "          <span class=\"eo\"><a href=\"https://github.com/Ranks/emojione/tree/2.2.7/assets/svg/$tmp0-$tmp1.svg\">&#x" . $c[0][0] . ";&#x200D;&#x" . $c[0][1] . ";</a></span>\n";
             } else {
               print "          <span class=\"eo\">&#x" . $c[0][0] . ";&#x200D;&#x" . $c[0][1] . ";</span>\n";
             }
@@ -136,7 +136,7 @@
               if ($tmp2[0] == "0") {
                 $tmp2 = substr($tmp2, 1);
               }
-              print "          <span class=\"eo\"><a href=\"https://github.com/Ranks/emojione/tree/master/assets/svg/$tmp0-$tmp1-$tmp2.svg\">&#x" . $c[0][0] . ";&#x200D;&#x" . $c[0][1] . ";&#x" . $c[0][2] . ";</a></span>\n";
+              print "          <span class=\"eo\"><a href=\"https://github.com/Ranks/emojione/tree/2.2.7/assets/svg/$tmp0-$tmp1-$tmp2.svg\">&#x" . $c[0][0] . ";&#x200D;&#x" . $c[0][1] . ";&#x" . $c[0][2] . ";</a></span>\n";
             } else {
               print "          <span class=\"eo\">&#x" . $c[0][0] . ";&#x200D;&#x" . $c[0][1] . ";&#x200D;&#x" . $c[0][2] . ";</span>\n";
             }
@@ -206,12 +206,12 @@
         if ($tmp[0] == "0") {
           $tmp = substr($tmp, 1);
         }
-        print "          <td><span class=\"eo\"><a href=\"https://github.com/Ranks/emojione/tree/master/assets/svg/$tmp.svg\">&#x" . $c[0][0] . ";</a></span></td>\n";
-        print "          <td><span class=\"eo\"><a href=\"https://github.com/Ranks/emojione/tree/master/assets/svg/$tmp-1f3fb.svg\">&#x" . $c[0][0] . ";&#x200D;&#x1F3FB;</a></span></td>\n";
-        print "          <td><span class=\"eo\"><a href=\"https://github.com/Ranks/emojione/tree/master/assets/svg/$tmp-1f3fc.svg\">&#x" . $c[0][0] . ";&#x200D;&#x1F3FC;</a></span></td>\n";
-        print "          <td><span class=\"eo\"><a href=\"https://github.com/Ranks/emojione/tree/master/assets/svg/$tmp-1f3fd.svg\">&#x" . $c[0][0] . ";&#x200D;&#x1F3FD;</a></span></td>\n";
-        print "          <td><span class=\"eo\"><a href=\"https://github.com/Ranks/emojione/tree/master/assets/svg/$tmp-1f3fe.svg\">&#x" . $c[0][0] . ";&#x200D;&#x1F3FE;</a></span></td>\n";
-        print "          <td><span class=\"eo\"><a href=\"https://github.com/Ranks/emojione/tree/master/assets/svg/$tmp-1f3ff.svg\">&#x" . $c[0][0] . ";&#x200D;&#x1F3FF;</a></span></td>\n";
+        print "          <td><span class=\"eo\"><a href=\"https://github.com/Ranks/emojione/tre/2.2.7/assets/svg/$tmp.svg\">&#x" . $c[0][0] . ";</a></span></td>\n";
+        print "          <td><span class=\"eo\"><a href=\"https://github.com/Ranks/emojione/tre/2.2.7/assets/svg/$tmp-1f3fb.svg\">&#x" . $c[0][0] . ";&#x200D;&#x1F3FB;</a></span></td>\n";
+        print "          <td><span class=\"eo\"><a href=\"https://github.com/Ranks/emojione/tre/2.2.7/assets/svg/$tmp-1f3fc.svg\">&#x" . $c[0][0] . ";&#x200D;&#x1F3FC;</a></span></td>\n";
+        print "          <td><span class=\"eo\"><a href=\"https://github.com/Ranks/emojione/tre/2.2.7/assets/svg/$tmp-1f3fd.svg\">&#x" . $c[0][0] . ";&#x200D;&#x1F3FD;</a></span></td>\n";
+        print "          <td><span class=\"eo\"><a href=\"https://github.com/Ranks/emojione/tre/2.2.7/assets/svg/$tmp-1f3fe.svg\">&#x" . $c[0][0] . ";&#x200D;&#x1F3FE;</a></span></td>\n";
+        print "          <td><span class=\"eo\"><a href=\"https://github.com/Ranks/emojione/tre/2.2.7/assets/svg/$tmp-1f3ff.svg\">&#x" . $c[0][0] . ";&#x200D;&#x1F3FF;</a></span></td>\n";
         print "        </tr>\n";
       }
       print "      </table>\n";
@@ -229,6 +229,7 @@
       array("Símbolos alfanuméricos con círculo alrededor",      "alfanum-circulo",        "U02460.pdf", 1, "2460",  "24FF"),
       array("Cajas",                                             "cajas",                  "U02500.pdf", 1, "2500",  "257F"),
       array("Formas geométricas",                                "formas-geometricas",     "U025A0.pdf", 1, "25A0",  "25FF"),
+      array("Símbolos misceláneos",                               "simbolos-misc",          "U02600.pdf", 1, "2600",  "26FF"),
       array("Dingbats",                                          "dingbats",               "U02700.pdf", 1, "2700",  "27BF"),
       array("Flechas suplementarias B",                          "flechas-suplementarias", "U02900.pdf", 1, "2900",  "297F"),
       array("Símbolos y flechas misceláneos",                    "simbolos-flechas",       "U02B00.pdf", 1, "2B00",  "2BEF"),
@@ -254,9 +255,9 @@
 
     $grupos_modificadores = array(
       // array("Banderas",                                          "banderas",           "",           2, "1F1E6", "1F1FF"),
-      //      array("Colores de piel",                                   "colores-piel",       "",           2, "0261D", "1F9FF"),
-//           array("Otros",                                             "otros",           "",           2, "0002A", "1F4FF"),
-           array("Otros",                                             "otros",           "",           3, "1F468", "1F469"),
+       array("Colores de piel",                                   "colores-piel",       "",           2, "0261D", "1F9FF"),
+      // array("Otros",                                             "otros",           "",           3, "1F468", "1F469"),
+      // array("Otros",                                             "otros",           "",           2, "0002A", "1F4FF"),
     );
 
     $grupos_restos = array(
@@ -265,7 +266,7 @@
 
     // genera_grupos($grupos_simbolos);
     // genera_grupos($grupos_dibujos);
-      genera_grupos($grupos_modificadores);
+     genera_grupos($grupos_modificadores);
     // genera_tabla_colores_piel("Colores de piel", "colores-piel", "", 1, "0261D", "1F9FF");
     //      genera_grupos($grupos_restos);
 
