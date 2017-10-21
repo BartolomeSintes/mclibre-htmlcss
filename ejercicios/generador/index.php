79df8d49f23c3aa77ba0e0ec562f42db03945531
BartolomeSintes <bartolome.sintes@gmail.com>
2017-10-21 22:15:59 +0200

    Corrijo enlaces (_ por -)

--- ejercicios/generador/index.php
+++ ejercicios/generador/index.php
@@ -16,9 +16,9 @@ include "generador_biblioteca.php";
 
     <nav>
       <p>
-        <a href="../../index.html"><img src="../../varios/iconos/icono_html5.svg" alt="Índice de HTML/CSS" title="Índice de HTML/CSS" height="48" width="42" /></a>
-        <a href="#"><img src="../../varios/iconos/icono_arrow_circle_up.svg" alt="Principio de la página" title="Principio de la página" height="36" width="36" /></a>
-        <a href="../index.html"><img src="../../varios/iconos/icono_ejercicios.svg" alt="Índice de ejercicios" title="Índice de ejercicios" width="36" height="36" /></a>
+        <a href="../../index.html"><img src="../../varios/iconos/icono-html5.svg" alt="Índice de HTML/CSS" title="Índice de HTML/CSS" height="48" width="42" /></a>
+        <a href="#"><img src="../../varios/iconos/icono-arrow-circle-up.svg" alt="Principio de la página" title="Principio de la página" height="36" width="36" /></a>
+        <a href="../index.html"><img src="../../varios/iconos/icono-ejercicios.svg" alt="Índice de ejercicios" title="Índice de ejercicios" width="36" height="36" /></a>
       </p>
 
       <div class="toc">
@@ -26,14 +26,14 @@ include "generador_biblioteca.php";
       </div>
     </nav>
 
-    <p><img src="../../varios/iconos/icono_en_construccion.svg" alt="En construcción" title="En construcción" width="55" height="48" />Este generador de ejercicios está en elaboración. El objetivo es poder elegir las etiquetas HTML y propiedades CSS que aparecen en los ejercicios.</p>
+    <p><img src="../../varios/iconos/icono-en-construccion.svg" alt="En construcción" title="En construcción" width="55" height="48" />Este generador de ejercicios está en elaboración. El objetivo es poder elegir las etiquetas HTML y propiedades CSS que aparecen en los ejercicios.</p>
 
     <hr />
 
     <form action="generador_html.php" method="<?= $metodo?>">
       <?php
   if (isset($_SESSION["error"])) {
-    print "    <p><img src=\"../../varios/iconos/icono_warning.svg\" alt=\"¡Aviso!\" title=\"¡Aviso!\" width=\"55\" height=\"48\" />$_SESSION[error]</p>\n\n";
+    print "    <p><img src=\"../../varios/iconos/icono-warning.svg\" alt=\"¡Aviso!\" title=\"¡Aviso!\" width=\"55\" height=\"48\" />$_SESSION[error]</p>\n\n";
   }
       ?>
 
@@ -111,7 +111,7 @@ include "generador_biblioteca.php";
     <footer>
       <p class="ultmod">Última modificación de esta página: 17 de junio de 2016</p>
 
-      <p class="licencia"><a rel="license" href="https://creativecommons.org/licenses/by-sa/4.0/deed.es_ES"><img src="../../varios/iconos/icono_cc_by_sa.svg" alt="Licencia Creative Commons" title="Licencia Creative Commons BY-SA" width="120" height="42" /></a><br />
+      <p class="licencia"><a rel="license" href="https://creativecommons.org/licenses/by-sa/4.0/deed.es_ES"><img src="../../varios/iconos/icono-cc-by-sa.svg" alt="Licencia Creative Commons" title="Licencia Creative Commons BY-SA" width="120" height="42" /></a><br />
         Esta página forma parte del curso <strong>Páginas web HTML / XHTML y hojas de estilo CSS</strong> por <a href="http://www.mclibre.org/" rel="author">Bartolomé Sintes Marco</a><br />
         que se distribuye bajo una <a rel="license" href="https://creativecommons.org/licenses/by-sa/4.0/deed.es_ES">Licencia Creative Commons Reconocimiento-CompartirIgual 4.0 Internacional (CC BY-SA 4.0)</a>.</p>
     </footer>
