1d9fc1dae85b608697f49c4c587f56d1cb387cc8
BartolomeSintes <bartolome.sintes@gmail.com>
2018-08-07 00:20:55 +0200

    Añado www.mclibre.org a title de head de todas las páginas

--- ejercicios/generador/index.php
+++ ejercicios/generador/index.php
@@ -5,7 +5,7 @@ include "generador_biblioteca.php";
 <html lang="es">
 <head>
   <meta charset="utf-8" />
-  <title>Generador de ejercicios. Páginas web HTML y hojas de estilo CSS. Bartolomé Sintes Marco</title>
+  <title>Generador de ejercicios. Páginas web HTML y hojas de estilo CSS. Bartolomé Sintes Marco. www.mclibre.org</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link href="../../varios/htmlcss.css" rel="stylesheet" type="text/css" title="Color" />
   <link rel="icon" href="../../varios/favicon.ico" />
