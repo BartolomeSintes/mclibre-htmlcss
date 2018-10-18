d6e2d6f68319144bc781aa497923788d7e24b98d
BartolomeSintes <bartolome.sintes@gmail.com>
2018-10-18 10:42:04 +0200

    Añado ejemplos input month week datetime-local en HTML Formularios

---
+++ html/ejemplos/formularios/formularios-datetime-local-1.php
@@ -0,0 +1,30 @@
+﻿<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <title>datetime-local. Formularios. HTML. Páginas web HTML y hojas de estilo CSS. Bartolomé Sintes Marco. www.mclibre.org</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+<?php
+function recoge($var)
+{
+    $tmp = (isset($_REQUEST[$var]))
+        ? trim(htmlspecialchars($_REQUEST[$var], ENT_QUOTES, "UTF-8"))
+        : "";
+    return $tmp;
+}
+
+$dato = recoge("dato");
+
+if ($dato == "") {
+    print "  <p>No ha indicado ninguna fecha y hora.</p>\n";
+} else {
+    print "  <p>La fecha indicada es <strong>$dato</strong>.</p>\n";
+}
+print "\n";
+?>
+  <p><a href="formularios-datetime-local-1.html">Volver al formulario.</a></p>
+</body>
+</html>
---
+++ html/ejemplos/formularios/formularios-month-1.php
@@ -0,0 +1,30 @@
+﻿<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <title>month. Formularios. HTML. Páginas web HTML y hojas de estilo CSS. Bartolomé Sintes Marco. www.mclibre.org</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+<?php
+function recoge($var)
+{
+    $tmp = (isset($_REQUEST[$var]))
+        ? trim(htmlspecialchars($_REQUEST[$var], ENT_QUOTES, "UTF-8"))
+        : "";
+    return $tmp;
+}
+
+$dato = recoge("dato");
+
+if ($dato == "") {
+    print "  <p>No ha indicado ningún mes.</p>\n";
+} else {
+    print "  <p>El mes indicado es <strong>$dato</strong>.</p>\n";
+}
+print "\n";
+?>
+  <p><a href="formularios-month-1.html">Volver al formulario.</a></p>
+</body>
+</html>
