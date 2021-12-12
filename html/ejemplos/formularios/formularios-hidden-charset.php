791c03e4c547430a7939ba1b45ce7ad8fdb2e9d0
BartolomeSintes <bartolome.sintes@gmail.com>
2021-12-13 00:12:09 +0100

    Secuencias Unicode: unifico explciaciones y completo apartado parejas de enamorados

---
+++ html/ejemplos/formularios/formularios-hidden-charset.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <title>hidden _charset_. Formularios. HTML. Páginas web HTML y hojas de estilo CSS. Bartolomé Sintes Marco. www.mclibre.org</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+<?php
+print "<pre>\n";
+print_r($_REQUEST);
+print "</pre>\n";
+?>
+  <p><a href="formularios-hidden-1.html">Volver al formulario.</a></p>
+</body>
+</html>
