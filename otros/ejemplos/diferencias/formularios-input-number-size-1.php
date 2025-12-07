787ec852290fe8499ce033385fb39f4ed2e7866b
BartolomeSintes <bartolome.sintes@gmail.com>
2025-12-07 10:08:15 +0100

    Diferencias entre GC/FF: atributo size en input numérico

---
+++ otros/ejemplos/diferencias/formularios-input-number-size-1.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <title>size. number. Formularios. HTML. Páginas web HTML y hojas de estilo CSS. Bartolomé Sintes Marco. www.mclibre.org</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+<?php
+function recoge($var)
+{
+    if (!isset($_REQUEST[$var])) {
+        $tmp = "";
+    } elseif (!is_array($_REQUEST[$var])) {
+        $tmp = trim(htmlspecialchars($_REQUEST[$var]));
+    } else {
+        $tmp = $_REQUEST[$var];
+        array_walk_recursive($tmp, function (&$valor) {
+            $valor = trim(htmlspecialchars($valor));
+        });
+    }
+    return $tmp;
+}
+
+$numero = recoge("numero");
+
+if ($numero == "") {
+    print "  <p>No ha indicado ningún número.</p>\n";
+} else {
+    print "  <p>El número indicado es <strong>$numero</strong>.</p>\n";
+}
+print "\n";
+print "  <p><a href=\"formularios-input-number-size-1.html\">Volver al formulario.</a></p>\n";
+?>
+</body>
+</html>
