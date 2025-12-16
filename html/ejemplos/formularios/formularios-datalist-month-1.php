0e9b9b83d99d2cb6b9e8dfbd4617411e8346fc9d
BartolomeSintes <bartolome.sintes@gmail.com>
2025-12-16 21:17:43 +0100

    HTML Formularios (2): incluyo elemento datalist

---
+++ html/ejemplos/formularios/formularios-datalist-month-1.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <title>datalist y month (1). Formularios. HTML. Páginas web HTML y hojas de estilo CSS. Bartolomé Sintes Marco. www.mclibre.org</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+<?php
+function recoge($var)
+{
+    if (!isset($_REQUEST[$var])) {
+        $tmp = "";
+    } elseif (!is_array($_REQUEST[$var])) {
+        $tmp = trim(htmlspecialchars($_REQUEST[$var]));
+    } else {
+        $tmp = $_REQUEST[$var];
+        array_walk_recursive($tmp, function (&$valor) {
+            $valor = trim(htmlspecialchars($valor));
+        });
+    }
+    return $tmp;
+}
+
+$favorito = recoge("favorito");
+
+if ($favorito == "") {
+    print "  <p>No ha escrito ningún mes.</p>\n";
+} else {
+    print "  <p>Su mes favorito es <strong>$favorito</strong>.</p>\n";
+}
+print "\n";
+?>
+  <p><a href="formularios-datalist-month-1.html">Volver al formulario.</a></p>
+</body>
+</html>
---
+++ html/ejemplos/formularios/formularios-datalist-text-2.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <title>datalist y text (2). Formularios. HTML. Páginas web HTML y hojas de estilo CSS. Bartolomé Sintes Marco. www.mclibre.org</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+<?php
+function recoge($var)
+{
+    if (!isset($_REQUEST[$var])) {
+        $tmp = "";
+    } elseif (!is_array($_REQUEST[$var])) {
+        $tmp = trim(htmlspecialchars($_REQUEST[$var]));
+    } else {
+        $tmp = $_REQUEST[$var];
+        array_walk_recursive($tmp, function (&$valor) {
+            $valor = trim(htmlspecialchars($valor));
+        });
+    }
+    return $tmp;
+}
+
+$favorito = recoge("favorito");
+
+if ($favorito == "") {
+    print "  <p>No ha escrito ningún color.</p>\n";
+} else {
+    print "  <p>Su color favorito es el <strong>$favorito</strong>.</p>\n";
+}
+print "\n";
+?>
+  <p><a href="formularios-datalist-text-2.html">Volver al formulario.</a></p>
+</body>
+</html>
